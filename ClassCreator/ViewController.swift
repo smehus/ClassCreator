9643717185df3d31aed457aea9a6d0b029ff55e4
Scott <scott.mehus@possible.com>
2019-08-27 19:54:06 -0600

    Works for managed objects

--- ClassCreator/ViewController.swift
+++ ClassCreator/ViewController.swift
@@ -9,15 +9,58 @@
 import UIKit
 import CoreData
 
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        ivars()
+        properties()
+    }
+}
 
+extension ViewController {
+    private func properties() {
+        var count: UInt32 = 0
+        let allocatedClass: AnyClass = objc_allocateClassPair(NSObject.classForCoder(), "ManagedFlyweight", 0)!
+        let props = class_copyPropertyList(Person.self, &count)!
+
+        for prop in UnsafeBufferPointer(start: props, count: Int(count)) {
+            let namePointer = property_getName(prop)
+            let name = String(cString: namePointer)
+            print("name: \(name)")
+
+            let encoding = "@"
+            var size = 0
+            var alignment = 0
+            NSGetSizeAndAlignment(encoding, &size, &alignment)
+            class_addIvar(allocatedClass, name, size, UInt8(alignment), encoding)
+        }
+
+        free(props)
+
+        objc_registerClassPair(allocatedClass)
+        let flyweight = allocatedClass.alloc() as! NSObject
+        flyweight.setValue("Scott", forKey: "firstName")
+        flyweight.setValue("Mehus", forKey: "lastName")
+
+        print("firstName: \(flyweight.value(forKey: "firstName")!) lastName: \(flyweight.value(forKey: "lastName")!)")
+
+    }
+}
+
+class Dog {
+    var name: String!
+    var breed: String!
+}
+
+extension ViewController {
+    private func ivars() {
         var count: UInt32 = 0
         let allocatedClass: AnyClass = objc_allocateClassPair(NSObject.classForCoder(), "Flyweight", 0)!
-        let ivars = class_copyIvarList(Person.self, &count)!
-        
+        let ivars = class_copyIvarList(Dog.self, &count)!
+
 
         for ivar in UnsafeBufferPointer(start: ivars, count: Int(count)) {
             let namePointer = ivar_getName(ivar)!
@@ -35,10 +78,10 @@ class ViewController: UIViewController {
 
         objc_registerClassPair(allocatedClass)
         let flyweight = allocatedClass.alloc() as! NSObject
-        flyweight.setValue("Scott", forKey: "firstName")
-        flyweight.setValue("Mehus", forKey: "lastName")
+        flyweight.setValue("Ginger", forKey: "name")
+        flyweight.setValue("Poodle", forKey: "breed")
 
-        print("firstName: \(flyweight.value(forKey: "firstName")) lastName: \(flyweight.value(forKey: "lastName"))")
+        print("name: \(flyweight.value(forKey: "name")!) breed: \(flyweight.value(forKey: "breed")!)")
     }
 }
 
