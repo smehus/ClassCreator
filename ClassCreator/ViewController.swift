58307ba61c26d5a5aca4a10ec1e77d384fad55c1
Scott <scott.mehus@possible.com>
2019-09-02 14:19:40 -0600

    Swizzling works here

--- ClassCreator/ViewController.swift
+++ ClassCreator/ViewController.swift
@@ -11,9 +11,26 @@ import CoreData
 
 class ViewController: UIViewController {
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        let originalSelector = #selector(ViewController.viewWillAppear(_:))
+        let replacementSelector = #selector(myViewWillAppear(animated:))
 
+        let originalMethod = class_getInstanceMethod(ViewController.self, originalSelector)
+        let replacementMethod = class_getInstanceMethod(type(of: self), replacementSelector)
+
+        let didAddMethod = class_addMethod(ViewController.self, originalSelector, method_getImplementation(replacementMethod!), method_getTypeEncoding(replacementMethod!))
+
+        if didAddMethod {
+            class_replaceMethod(ViewController.self, replacementSelector, method_getImplementation(originalMethod!), method_getTypeEncoding(originalMethod!))
+        } else {
+            method_exchangeImplementations(originalMethod!, replacementMethod!);
+        }
+    }
+
+    @objc func myViewWillAppear(animated: Bool) {
+        print("*** hYEYEYEYEYEYOO IT WORKS")
     }
 
     override func viewWillAppear(_ animated: Bool) {
--- ClassCreatorTests/ClassCreatorTests.swift
+++ ClassCreatorTests/ClassCreatorTests.swift
@@ -27,13 +27,14 @@ class ClassCreatorTests: XCTestCase {
     }
 
     func test_swizzle() {
-        let originalSelector = #selector(UIViewController.viewWillAppear(_:))
+        let originalSelector = #selector(ViewController.viewWillAppear(_:))
         let replacementSelector = #selector(myViewWillAppear(animated:))
 
         let originalMethod = class_getInstanceMethod(ViewController.self, originalSelector)
         let replacementMethod = class_getInstanceMethod(type(of: self), replacementSelector)
 
-        let didAddMethod = class_addMethod(ViewController.self, replacementSelector, method_getImplementation(replacementMethod!), method_getTypeEncoding(replacementMethod!))
+        let didAddMethod = class_addMethod(ViewController.self, originalSelector, method_getImplementation(replacementMethod!), method_getTypeEncoding(replacementMethod!))
+        
         if didAddMethod {
             class_replaceMethod(ViewController.self, replacementSelector, method_getImplementation(originalMethod!), method_getTypeEncoding(originalMethod!))
         } else {
@@ -41,7 +42,7 @@ class ClassCreatorTests: XCTestCase {
         }
     }
 
-    func myViewWillAppear(animated: Bool) {
+    @objc func myViewWillAppear(animated: Bool) {
         print("*** hYEYEYEYEYEYOO IT WORKS")
     }
 
