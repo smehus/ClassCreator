990f3709f4f1b003543c141f126f63875934b93f
Scott <scott.mehus@possible.com>
2019-09-02 12:42:59 -0600

    Updates

--- ClassCreator/ViewController.swift
+++ ClassCreator/ViewController.swift
@@ -33,10 +33,10 @@ extension ViewController {
 
         guard let person = try? CoreDataManager.shared.managedContext.fetch(request).first else { fatalError() }
 
-        let creator: FlyweightCreator<Person> = FlyweightCreator(className: "PersonFlyweight", inspectionType: .property)
+        let creator: FlyweightCreator<Person> = FlyweightCreator(className: "PersonFlyweight")
         let flyweight = creator.generate(from: person)
 
-        print("firstName: \(flyweight.firstName!) lastName: \(flyweight.lastName!)")
+//        print("firstName: \(flyweight.firstName!) lastName: \(flyweight.lastName!)")
 
     }
 }
@@ -53,76 +53,83 @@ extension ViewController {
         dog.species = "Poodle"
         dog.name = "Scott"
 
-        let creator: FlyweightCreator<Dog> = FlyweightCreator(className: "DogFlyweight", inspectionType: .ivar)
+        let creator: FlyweightCreator<Dog> = FlyweightCreator(className: "DogFlyweight")
         let flyweight = creator.generate(from: dog)
 
-        print("name: \(flyweight.name!) species: \(flyweight.species!)")
+//        print("name: \(flyweight.name!) species: \(flyweight.species!)")
     }
 }
 
 struct FlyweightCreator<T: AnyObject> {
 
-    enum ClassInspectionType {
-        case ivar
-        case property
-    }
-
-    private let inspectionType: ClassInspectionType
     private let className: String
 
-    init(className: String, inspectionType: ClassInspectionType) {
+    init(className: String) {
         self.className = className
-        self.inspectionType = inspectionType
     }
 
     func generate(from object: AnyObject) -> Flyweight {
-        var count: UInt32 = 0
+        var propertyCount: UInt32 = 0
+        var ivarCount: UInt32 = 0
+        let encoding = "@"
+
         // Create class
         let allocatedClass: AnyClass = objc_allocateClassPair(Flyweight.classForCoder(), className, 0)!
 
-        // Get all ivars out of input class
-        let ivars = inspectionType == .ivar ? class_copyIvarList(T.self, &count)! : class_copyPropertyList(T.self, &count)
-        defer { free(ivars) }
 
+        // Get all properties
+        let propertyList = class_copyPropertyList(T.self, &propertyCount)
+        defer { free(propertyList) }
 
-        // Copy over ivars from input class into created class
-        for ivar in UnsafeBufferPointer(start: ivars, count: Int(count)) {
-            let namePointer = inspectionType == .ivar ? ivar_getName(ivar)! : property_getName(ivar)
+        for property in UnsafeBufferPointer(start: propertyList, count: Int(propertyCount)) {
+            let namePointer = property_getName(property)
             let name = String(cString: namePointer)
-            print("name: \(name)")
+            print("*** Property Name \(name)")
 
-            let encoding = "@"
-            var size = 0
-            var alignment = 0
-            NSGetSizeAndAlignment(encoding, &size, &alignment)
-            class_addIvar(allocatedClass, name, size, UInt8(alignment), encoding)
+
+            var ivarSize = 0
+            var ivarAlignment = 0
+            NSGetSizeAndAlignment(encoding, &ivarSize, &ivarAlignment)
+            class_addIvar(allocatedClass, name, ivarSize, UInt8(ivarAlignment), encoding)
         }
 
+        let ivarList = class_copyIvarList(T.self, &ivarCount)
+        defer { free(ivarList) }
 
+        for ivar in UnsafeBufferPointer(start: ivarList, count: Int(ivarCount)) {
+            guard let namePointer = ivar_getName(ivar) else { continue }
+            let name = String(cString: namePointer)
+            print("*** Ivar Name \(name)")
+
+            var ivarSize = 0
+            var ivarAlignment = 0
+            NSGetSizeAndAlignment(encoding, &ivarSize, &ivarAlignment)
+            class_addIvar(allocatedClass, name, ivarSize, UInt8(ivarAlignment), encoding)
+        }
 
 //        class_addProtocol(allocatedClass, Mamal.self)
 
         // Register class
         objc_registerClassPair(allocatedClass)
 
+
         // Creat instance of new class
         let instance = allocatedClass.alloc() as! Flyweight
 
+
         // Populate new class ivars with input instance
-        for ivar in UnsafeBufferPointer(start: ivars, count: Int(count)) {
+        for ivar in UnsafeBufferPointer(start: ivarList, count: Int(ivarCount)) {
             let namePointer = property_getName(ivar)
             let name = String(cString: namePointer)
 
             // Get Dog Speciies value
             let objectIvar = class_getProperty(T.self, name)!
-            print("*** \(inspectionType) \(objectIvar)")
             let objectValue = property_getAttributes(objectIvar)
-
 
             // Set Dog Speciies Value on the flyweight
 
             let flyweightIvar = class_getInstanceVariable(allocatedClass, name)!
-             object_setIvar(instance, flyweightIvar, objectValue)
+            object_setIvar(instance, flyweightIvar, objectValue)
         }
 
         return instance
